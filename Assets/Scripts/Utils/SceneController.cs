2c615fc0d6889b5a0dc92e888e48df512ec9f2ec
nam2671998 <namtsh@cscmobi.com>
2022-09-12 21:06:17 +0700

    Remove legacy code

--- Assets/Scripts/Utils/SceneController.cs
+++ Assets/Scripts/Utils/SceneController.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-// using UnityEngine.AddressableAssets;
-// using UnityEngine.ResourceManagement.ResourceProviders;
 using System.Threading.Tasks;
 using Pixelplacement;
 
@@ -22,86 +20,10 @@ public class SceneController : Singleton<SceneController>
         Application.targetFrameRate = 60;
 #endif
     }
-    public void LoadScene(string sceneName,  bool isCurrentAddressable = true, bool isNextAddressable = true)
+    public void LoadScene(string sceneName)
     {
         StartCoroutine(_LoadSceneNoAddressable(sceneName));
-        // if (isCurrentAddressable && isNextAddressable)
-        //     StartCoroutine(_LoadAddressableScene(sceneName));
-        // else if (!isCurrentAddressable && !isNextAddressable)
-        //     StartCoroutine(_LoadScene(sceneName));
-        // else if (!isCurrentAddressable && isNextAddressable)
-        //     StartCoroutine(_LoadAddressableFromNormalScene(sceneName));
     }
-    // private IEnumerator _LoadAddressableScene(string sceneName)//use this function to load between addressable scene
-    // {
-    //     var delay = new WaitForSecondsRealtime(0.06f);
-    //     var loadingAsyncTask = Addressables.LoadSceneAsync("Loading", LoadSceneMode.Additive);
-    //     while (!loadingAsyncTask.IsDone)
-    //         yield return delay;
-    //     var loadController = FindObjectOfType<LoadSceneController>();
-    //     yield return new WaitForSecondsRealtime(0.4f);
-    //     var asyncTask = Addressables.UnloadSceneAsync(currentScene);
-    //     yield return new WaitForSecondsRealtime(0.1f);
-    //     float progress = 0;
-    //     while (!asyncTask.IsDone && progress < 0.5f)
-    //     {
-    //         yield return delay;
-    //         progress = Mathf.Min(progress + 0.06f, asyncTask.PercentComplete / 2f);
-    //         loadController.UpdateLoadingBar(progress);
-    //     }
-    //     asyncTask = Addressables.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-    //     while (!asyncTask.IsDone)
-    //     {
-    //         yield return delay;
-    //         loadController.UpdateLoadingBar((1 + asyncTask.PercentComplete) / 2f);
-    //     }
-    //     currentScene = asyncTask.Result;
-    //     currentScene.ActivateAsync();
-    //     loadController.FadeOut();
-    //     yield return new WaitForSecondsRealtime(0.5f);
-    //     Addressables.UnloadSceneAsync(loadingAsyncTask.Result);
-    // }
-    // private IEnumerator _LoadScene(string sceneName)//use this function to load between a normal scene
-    // {
-    //     var delay = new WaitForSecondsRealtime(0.06f);
-    //     string currentSceneName = SceneManager.GetActiveScene().name;
-    //     var loadingAsyncTask = Addressables.LoadSceneAsync("Loading", LoadSceneMode.Additive);
-    //     while (!loadingAsyncTask.IsDone)
-    //         yield return delay;
-    //     var loadController = FindObjectOfType<LoadSceneController>();
-    //     yield return new WaitForSecondsRealtime(0.4f);
-    //     var asyncTask = SceneManager.UnloadSceneAsync(currentSceneName);
-    //     yield return new WaitForSecondsRealtime(0.1f);
-    //     float progress = 0;
-    //     while (!asyncTask.isDone && progress < 0.5f)
-    //     {
-    //         yield return delay;
-    //         progress = Mathf.Min(progress + 0.06f, asyncTask.progress / 2f);
-    //         loadController.UpdateLoadingBar(progress);
-    //     }
-    //     asyncTask = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-    //     while (!asyncTask.isDone)
-    //     {
-    //         loadController.UpdateLoadingBar(0.5f + asyncTask.progress / 2);
-    //         yield return delay;
-    //     }
-    //     SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
-    //     loadController.FadeOut();
-    //     yield return new WaitForSecondsRealtime(0.5f);
-    //     Addressables.UnloadSceneAsync(loadingAsyncTask.Result);
-    // }
-    // private IEnumerator _LoadAddressableFromNormalScene(string sceneName)//use this function to load addressable scene from normal one
-    // {
-    //     var delay = new WaitForSecondsRealtime(0.06f);
-    //     string currentSceneName = SceneManager.GetActiveScene().name;
-    //     var asyncTask = Addressables.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-    //     while (!asyncTask.IsDone)
-    //         yield return delay;
-    //     currentScene = asyncTask.Result;
-    //     currentScene.ActivateAsync();
-    //     SceneManager.UnloadSceneAsync(currentSceneName);
-    // }
-
     private IEnumerator _LoadSceneNoAddressable(string sceneName)//use this function to load between a normal scene
     {
         var delay = new WaitForSecondsRealtime(0.1f);
